47a7a19969e6c3afe5a08037b3230a03806a1dff
Hadeslibo <benimdevelop@gmail.com>
2025-07-20 23:29:59 +0300

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/binance-api/, ''),
         secure: true,
         headers: {
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
           'Accept': 'application/json, text/plain, */*',
           'Accept-Language': 'en-US,en;q=0.9',
           'DNT': '1',
@@ -26,6 +27,7 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/binance-testnet/, ''),
         secure: true,
         headers: {
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
           'Accept': 'application/json, text/plain, */*',
           'Accept-Language': 'en-US,en;q=0.9',
           'DNT': '1',
